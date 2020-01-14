cb3fe46bffb1f805283dbedce09602e88e249fdc
Yamoun955 <56540938+Yamoun955@users.noreply.github.com>
2020-01-14 22:16:25 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,149 @@
+$(document).ready(function(){
+   
+$("#j1").click(function(){
+    
+   $(".col").animate({
+       left:"0px"},250,function(){ $("#j1").animate({width:"420px"},250)});
+   $(".col").animate({
+       left:"0px"},250,function(){ $("#j2").animate({width:"140px"},250)});
+   $(".col").animate({
+       left:"0px"},250,function(){ $("#j3").animate({width:"140px"},250)});
+           $(".col").animate({
+       left:"0px"},250,function(){ $("#j4").animate({width:"140px"},250)});
+      $(".col").animate({
+       left:"0px"},250,function(){ $("#j5").animate({width:"140px"},250)});
+  
+    });
+    
+
+
+    
+$("#j2").click(function(){
+    
+   $(".col").animate({
+       left:"0px"},250,function(){ $("#j2").animate({width:"420px"},250)});
+         $(".col").animate({
+       left:"0px"},250,function(){ $("#j1").animate({width:"140px"},250)});
+      $(".col").animate({
+       left:"0px"},250,function(){ $("#j3").animate({width:"140px"},250)});
+    $(".col").animate({
+       left:"0px"},250,function(){ $("#j4").animate({width:"140px"},250)});
+      $(".col").animate({
+       left:"0px"},250,function(){ $("#j5").animate({width:"140px"},250)});
+  
+    });
+    
+$("#j3").click(function(){
+    
+   $(".col").animate({
+       left:"0px"},250,function(){ $("#j3").animate({width:"420px"},250)});
+         $(".col").animate({
+       left:"0px"},250,function(){ $("#j1").animate({width:"140px"},250)});
+     $(".col").animate({
+       left:"0px"},250,function(){ $("#j2").animate({width:"140px"},250)});
+       $(".col").animate({
+       left:"0px"},250,function(){ $("#j4").animate({width:"140px"},250)});
+      $(".col").animate({
+       left:"0px"},250,function(){ $("#j5").animate({width:"140px"},250)});
+  
+    });
+    
+$("#j4").click(function(){
+    
+   $(".col").animate({
+       left:"0px"},250,function(){ $("#j4").animate({width:"420px"},250)});
+         $(".col").animate({
+       left:"0px"},250,function(){ $("#j1").animate({width:"140px"},250)});
+   $(".col").animate({
+       left:"0px"},250,function(){ $("#j2").animate({width:"140px"},250)});
+   $(".col").animate({
+       left:"0px"},250,function(){ $("#j3").animate({width:"140px"},250)});
+    $(".col").animate({
+       left:"0px"},250,function(){ $("#j5").animate({width:"140px"},250)});
+    });
+    
+    $("#j5").click(function(){
+    
+   $(".col").animate({
+       left:"0px"},250,function(){ $("#j5").animate({width:"420px"},250)});
+         $(".col").animate({
+       left:"0px"},250,function(){ $("#j1").animate({width:"140px"},250)});
+   $(".col").animate({
+       left:"0px"},250,function(){ $("#j2").animate({width:"140px"},250)});
+   $(".col").animate({
+       left:"0px"},250,function(){ $("#j3").animate({width:"140px"},250)});
+     $(".col").animate({
+       left:"0px"},250,function(){ $("#j4").animate({width:"140px"},250)});
+    });
+$("#sc").click(function(){
+       $(".col").animate({
+       left:"0px"},250,function(){ $("#j1").animate({width:"420px"},250)});
+   $(".col").animate({
+       left:"0px"},250,function(){ $("#j2").animate({width:"140px"},250)});
+   $(".col").animate({
+       left:"0px"},250,function(){ $("#j3").animate({width:"140px"},250)});
+           $(".col").animate({
+       left:"0px"},250,function(){ $("#j4").animate({width:"140px"},250)});
+      $(".col").animate({
+       left:"0px"},250,function(){ $("#j5").animate({width:"140px"},250)});
+    
+    
+});
+    
+$("#plm").click(function(){  
+       $(".col").animate({
+       left:"0px"},250,function(){ $("#j2").animate({width:"420px"},250)});
+         $(".col").animate({
+       left:"0px"},250,function(){ $("#j1").animate({width:"140px"},250)});
+      $(".col").animate({
+       left:"0px"},250,function(){ $("#j3").animate({width:"140px"},250)});
+    $(".col").animate({
+       left:"0px"},250,function(){ $("#j4").animate({width:"140px"},250)});
+      $(".col").animate({
+       left:"0px"},250,function(){ $("#j5").animate({width:"140px"},250)});
+});
+
+$("#alba").click(function(){
+   
+   $(".col").animate({
+       left:"0px"},250,function(){ $("#j3").animate({width:"420px"},250)});
+         $(".col").animate({
+       left:"0px"},250,function(){ $("#j1").animate({width:"140px"},250)});
+     $(".col").animate({
+       left:"0px"},250,function(){ $("#j2").animate({width:"140px"},250)});
+       $(".col").animate({
+       left:"0px"},250,function(){ $("#j4").animate({width:"140px"},250)});
+      $(".col").animate({
+       left:"0px"},250,function(){ $("#j5").animate({width:"140px"},250)});
+});
+ 
+    $("#rsh").click(function(){
+           $(".col").animate({
+       left:"0px"},250,function(){ $("#j4").animate({width:"420px"},250)});
+         $(".col").animate({
+       left:"0px"},250,function(){ $("#j1").animate({width:"140px"},250)});
+   $(".col").animate({
+       left:"0px"},250,function(){ $("#j2").animate({width:"140px"},250)});
+   $(".col").animate({
+       left:"0px"},250,function(){ $("#j3").animate({width:"140px"},250)});
+    $(".col").animate({
+       left:"0px"},250,function(){ $("#j5").animate({width:"140px"},250)});
+    });
+    
+    
+    $("#mr").click(function(){
+           $(".col").animate({
+       left:"0px"},250,function(){ $("#j5").animate({width:"420px"},250)});
+         $(".col").animate({
+       left:"0px"},250,function(){ $("#j1").animate({width:"140px"},250)});
+   $(".col").animate({
+       left:"0px"},250,function(){ $("#j2").animate({width:"140px"},250)});
+   $(".col").animate({
+       left:"0px"},250,function(){ $("#j3").animate({width:"140px"},250)});
+     $(".col").animate({
+       left:"0px"},250,function(){ $("#j4").animate({width:"140px"},250)});
+        
+    });
+
+    
+});
